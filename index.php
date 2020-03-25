100b2c17397f1d83cec3393e8ceb2e4b00cbdd04
moto <moto1019@gmail.com>
2020-03-25 09:04:28 +0800

    運算子結束

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
 <?php
 
 echo "Hello World";
+$a=1;
+$a=2.1;
+echo "<br>";
+echo $a;
+
+// 宣告一個常數
+// 常數不可變更
+define("PI",3.1415927);
+echo PI;
+
+// 運算子++ => $b=$b+1
+
+$a="A";
+$b="B";
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+
+
+// 三元運算子
+$a=0;
+
+echo ($a>0)?"很貴":"免費";
+
+echo "<hr>";
+
+if($a>0){
+    echo "很貴";
+}
+else {
+    echo "free";
+}
 
 ?>
